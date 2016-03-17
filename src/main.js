78becd028a746c8fa6b64ee4bc9411b471c4fd19
林龙辉 <540886643@qq.com>
2016-03-17 12:02:24 +0800

    delete comment

--- src/main.js
+++ src/main.js
@@ -4,17 +4,9 @@ import { sync } from 'vuex-router-sync'
 import store from './vuex/store'
 import router from './router/index'
 
+// for debugging
 Vue.config.debug = true
-// before sync, store do not contain the actions
-console.log(store)  // -> undefined
-sync(store, router)
-// after sync, store also do not contain the actions
-console.log(store) // -> undefined
 
-/* eslint-disable no-new */
-new Vue({
-  store,
-  components: { App }
-})
+sync(store, router)
 
 router.start(App, '#app')
